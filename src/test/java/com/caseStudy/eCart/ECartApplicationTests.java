97ee5288eb4abfd8fb1f0339927a805482f2a7f8
MituJanavy <57383711+MituJanavy@users.noreply.github.com>
2020-04-03 20:55:16 +0530

    Update ECartApplicationTests.java

--- src/test/java/com/caseStudy/eCart/ECartApplicationTests.java
+++ src/test/java/com/caseStudy/eCart/ECartApplicationTests.java
@@ -1,10 +1,8 @@
 package com.caseStudy.eCart;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ECartApplicationTests {
